aa9dc398478f8a79a4a0ff32ada7bd5c2556ab98
hitendrarao <20615412+hitendrarao@users.noreply.github.com>
2022-05-11 20:47:46 +0530

    in progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
-const prettierConfig = require("./prettier.config")
-
 module.exports = {
-    root: true,
-    parser: "@babel/eslint-parser",
-    extends: [
-        "eslint:recommended",
-        "plugin:prettier/recommended",
-        "prettier",
-        "airbnb"
-    ],
-    plugins: ["prettier", "jest", "react-hooks"],
-    ignorePatterns: ["public/**/*", "app/build/*"],
-    rules: {
-        "prettier/prettier": ["error", prettierConfig],
-        "global-require": 0,
-        "linebreak-style": 0,
-        "no-global-assign": 0,
-        "no-console": 0,
-        "no-unused-vars": 1
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier', // Make sure this is the last
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
