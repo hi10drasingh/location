fda3d45f625db66789b1e8ffe3b18ee31733e790
hitendrarao <20615412+hitendrarao@users.noreply.github.com>
2022-05-12 14:34:50 +0530

    changed prettier json to js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-    'prettier', // Make sure this is the last
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    "global-require": 0,
-    "linebreak-style": 0,
-    "no-global-assign": 0,
-    "no-console": 0,
-    "no-unused-vars": 1
-  },
-};
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        "airbnb-base",
+        "prettier" // Make sure this is the last
+    ],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        "global-require": 0,
+        "linebreak-style": 0,
+        "no-global-assign": 0,
+        "no-console": 0,
+        "no-unused-vars": 1
+    }
+}
--- index.js
+++ index.js
@@ -1,96 +1,132 @@
-(function (root, factory) {
+const LocPlugin = (root, factory) => {
     if (typeof root.LocationPlugin === "undefined") {
-        root.LocationPlugin = factory(root);
+        return { LocationPlugin: factory(root), ...root }
     }
-})(typeof window !== "undefined" ? window : this, function (window) {
-    
 
-    const MAPS = "map";
-    const MODAL = "model";
+    return root
+}
+
+const factory = window => {
+    const MAP = "map"
+
+    const MODAL = "model"
 
     const TYPES = {
-        "JS": "js",
-        "CSS": "css"
+        JS: "js",
+        CSS: "css"
     }
 
-    const defaults = {
-        global: true,
-        popup: false,
-    };
+    // const defaults = {
+    //     global: true,
+    //     popup: false
+    // }
 
     const assetList = {
-        [MAPS]: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDcupH1pAzOULz35i00ADvn1sndjRa4m_A&libraries=places",
+        [MAP]: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDcupH1pAzOULz35i00ADvn1sndjRa4m_A&libraries=places",
         [MODAL]: [
             "/themes/beta/css/elements/plugins/location/style.css",
-            "/themes/beta/css/elements/plugins/location/icomoon/style.css",
-        ],
-    };
+            "/themes/beta/css/elements/plugins/location/icomoon/style.css"
+        ]
+    }
 
-    const extend = () => {
-        const merged = {};
-        Array.prototype.forEach.call(arguments, (obj) => {
-            for (const key in obj) {
-                if (!obj.hasOwnProperty(key)) return;
-                merged[key] = obj[key];
-            }
-        });
-        return merged;
-    };
+    // const extend = () => {
+    //     const merged = {}
+    //     Array.prototype.forEach.call(arguments, obj => {
+    //         for (const key in obj) {
+    //             if (!obj.hasOwnProperty(key)) return
+    //             merged[key] = obj[key]
+    //         }
+    //     })
+    //     return merged
+    // }
 
-    const emitEvent = (type, elem, detail) => {
-        // Create a new event
-        const event = new CustomEvent(type, {
-            bubbles: true,
-            cancelable: true,
-            detail,
-        });
+    // const emitEvent = (type, elem, detail) => {
+    //     // Create a new event
+    //     const event = new CustomEvent(type, {
+    //         bubbles: true,
+    //         cancelable: true,
+    //         detail
+    //     })
 
-        // Dispatch the event
-        elem.dispatchEvent(event);
-    };
+    //     // Dispatch the event
+    //     elem.dispatchEvent(event)
+    // }
 
-    const loadGoogleMapsJs = async () => {
-        if (typeof google === "undefined" || typeof google.maps === "undefined") {
-            // load maps script dynamically
-            await loadAssets(MAP);
+    const getParsedUrl = url => {
+        let parsedURL = url
+
+        const assetsURL = window.assets_url || ""
+
+        const assetsVersion = window.assets_version || 1
+
+        if (window.minify === 1) {
+            parsedURL = `${assetsURL}?f=${parsedURL}&v=${assetsVersion}`
+        } else {
+            parsedURL = assetsURL + parsedURL
         }
-    };
 
-    var loadAssets = async (type) => {
+        return parsedURL
+    }
+
+    const loadResource = async (type, url) => {
+        const parsedURL = getParsedUrl(url)
+        let resource = ""
+        switch (type) {
+            case TYPES.CSS:
+                resource = document.createElement("link")
+                resource.rel = "stylesheet"
+                resource.href = parsedURL
+                break
+
+            case TYPES.JS:
+                resource = document.createElement("script")
+                resource.type = "text/javascript"
+                resource.src = parsedURL
+                break
+
+            default:
+                break
+        }
+
+        document.head.appendChild(resource)
+    }
+
+    const loadAssets = async type => {
         switch (type) {
             case MODAL:
-                assetList[MODAL].css.forEach( async (style) => {
-                   await loadResource(style);
-                });
-                break;
+                assetList[MODAL].css.forEach(async style => {
+                    await loadResource(style)
+                })
+                break
 
             case MAP:
-                await loadResource(assetList[MAP].js);
-                break;
-        }
-    };
+                await loadResource(assetList[MAP].js)
+                break
 
-    var loadResource = async (type, url) => {
-        const assets_url = window.assets_url || '';
-        const assets_version = window.assets_version || 1;
-        if (window.minify === 1) {
-            url = `${assets_url  }?f=${url}&v=${assets_version}`;
-        } else {
-            url = assets_url + url;
-        }
-
-        switch (type) {
-            case TYPES.JS:
-
-                break;
-
-            case TYPES.CSS:
-
-                break;
+            default:
+                break
         }
     }
 
-    const constructor = (selector, options) => {};
+    const loadGoogleMapsJs = async () => {
+        if (
+            typeof google === "undefined" ||
+            typeof google.maps === "undefined"
+        ) {
+            // load maps script dynamically
+            await loadAssets(MAP)
+        }
+    }
 
-    return constructor;
-});
+    const constructor = (selector, options) => {}
+
+    const init = async () => {
+        loadGoogleMapsJs()
+
+        return constructor
+    }
+
+    return init()
+}
+
+window = LocPlugin(window, factory)
